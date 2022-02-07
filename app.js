1a208fdf008ac970a9780669d2c4b4d18180fcb3
Julie Branyan <juliebranyan@gmail.com>
2022-02-06 19:47:21 -0700

    custom error handling, template updates, and image resizing

--- app.js
+++ app.js
@@ -21,7 +21,6 @@ app.use('/static',express.static('public'));
 
 //render the home route
 app.get('/', (req, res, next) => {
-    console.log( projects );
     //Render the Home Page
     res.render('index', { projects });
 
@@ -30,16 +29,16 @@ app.get('/', (req, res, next) => {
 //render the about route
 app.get('/about', (req, res, next) => {
     console.log( projects );
-
+    err.status = 500;
     //Render the about Page
     res.render('about');
 });
 
 //render dynamic project routes to match to project or projects
 app.get('/projects/:id', (req, res, next) => {
-    const projectId = req.params.id;
-    console.log( projectId );
-    const project = projects.find( ({ id }) => id === +projectId );
+    //identify the project information based off the project id 
+    let projectId = req.params.id;
+    let project = projects.find( ({ id }) => id === projectId );
     //Render the Project Page
     res.render('project', { project });
 });
@@ -48,19 +47,20 @@ app.get('/projects/:id', (req, res, next) => {
 app.use((req, res, next) => {
     const err = new Error();
     err.status = 404;
-    err.message = `Recevied Error ${err.status}: Page not found. Contact Site Administrator at help@fakeemail.com`;
+    err.message = `Page not found.`;
+    res.render('page-not-found', { error: err });
     next(err);
 });
 
 //Global error handling
-function errorHandler (err, req, res, next) {
-    res.status(500)
-    res.render('error', { error: err })
-  }
+app.use((err, req, res, next) => {
+    err.message = `An unexpected error has occured.`;
+    res.status(500);
+    res.render('error', { error: err });
+  });
 
-//start the server and log an error message the app is 
+//start the server and log an error message the app is
 //listening to port 3000
 app.listen(3000, () => {
-    //console.log(projectId);
     console.log('The app is running on localhost: 3000');
 });
